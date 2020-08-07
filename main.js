7bc8bae780c0e886d325ef281302d330f495435f
Henrykvdb <henrykvdb@hotmail.com>
2020-08-08 00:37:59 +0200

    Update tile sprite and add background

--- main.js
+++ main.js
@@ -12,16 +12,18 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    
     this.load.image('tile', 'tile.png'); //359x 208y shift for one block shift
 }
 
 function create ()
 {
+    this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#D0EEFF");
+
     for(var j = 0; j<10; j++){
-        for(var i = 11; i<21-j; i++){
-            var sprite = this.add.sprite(0.1*359*(i-j), 0.1*208*(i+j), 'tile');
-            sprite.setScale(0.1);
-            
+        for(var i = 11; i<21; i++){
+            var sprite = this.add.sprite(0.26*147*(i-j), 0.26*85*(i+j), 'tile');
+            sprite.setScale(0.25);
         }
     }
 }
