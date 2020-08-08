da8ccc894b88398bcb0e553c92fa23a70ef6c918
Henrykvdb <henrykvdb@hotmail.com>
2020-08-08 12:10:08 +0200

    Update window size and add textures to code

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const sizeX = 1000;
+const sizeY = 600;
+
+//359x 208y for one block shift
+const shiftX = 147;
+const shiftY = 85;
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 800,
+    width: sizeX,
+    height: sizeY,
     scene: {
         preload: preload,
         create: create
@@ -10,19 +17,23 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var assets;
 function preload ()
 {
-    
-    this.load.image('tile', 'tile.png'); //359x 208y shift for one block shift
+    this.load.image('grass1', 'assets/tiles/grass1.png');
+    this.load.image('heighttile', 'assets/tiles/heighttile.png');
+    this.load.image('water1', 'assets/tiles/water1.png');
+    assets = ['grass1','heighttile','water1']
 }
 
 function create ()
 {
     this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#D0EEFF");
 
-    for(var j = 0; j<10; j++){
-        for(var i = 11; i<21; i++){
-            var sprite = this.add.sprite(0.26*147*(i-j), 0.26*85*(i+j), 'tile');
+    for(var j = -5; j<5; j++){
+        for(var i = -5; i<5; i++){
+            var asset = assets[Math.floor(Math.random() * assets.length)];
+            var sprite = this.add.sprite(sizeX/2 + 0.26*shiftX*(i-j), sizeY/2 + 0.26*shiftY*(i+j), asset);
             sprite.setScale(0.25);
         }
     }
