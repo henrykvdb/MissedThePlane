cee68c88bc807167663bac2d61ff2e532eb201fc
winand appels <winnie33winnie33@gmail.com>
2020-08-11 12:52:13 +0200

    UI now front layer

--- sceneGame.js
+++ sceneGame.js
@@ -27,14 +27,14 @@ class GameScene extends Phaser.Scene {
 
         // Next button
         var gameScene = this
-        this.btnNext = this.add.sprite(900, 100, 'btn_next').setScale(0.25).setInteractive();
+        this.btnNext = this.add.sprite(900, 100, 'btn_next').setScale(0.25).setInteractive().setDepth(100);
         this.btnNext.visible = false;
         this.btnNext.on('pointerdown', function (pointer) {
             gameScene.scene.restart({ levelIndex: ++gameScene.levelIndex })
         });
 
         // Restart button
-        this.btnRestart = this.add.sprite(900, 100, 'btn_restart').setScale(0.25).setInteractive();
+        this.btnRestart = this.add.sprite(900, 100, 'btn_restart').setScale(0.25).setInteractive().setDepth(100);
         this.btnRestart.on('pointerdown', function (pointer) {
             gameScene.scene.restart({ levelIndex: gameScene.levelIndex })
         });
@@ -42,7 +42,7 @@ class GameScene extends Phaser.Scene {
         // Draw sound change bar
         gameScene.graphics.fillStyle(0x000000, 0.4);
         var sliderTop = VOL_POS_Y - VOL_LENGTH - VOL_OFFSET;
-        gameScene.volumeSlider.push(gameScene.graphics.fillRect(VOL_POS_X - VOL_THICKNESS / 2, sliderTop, VOL_THICKNESS, VOL_LENGTH).setInteractive())
+        gameScene.volumeSlider.push(gameScene.graphics.fillRect(VOL_POS_X - VOL_THICKNESS / 2, sliderTop, VOL_THICKNESS, VOL_LENGTH).setInteractive().setDepth(100))
         gameScene.volumeSlider.push(gameScene.add.sprite(VOL_POS_X, sliderTop + VOL_LENGTH / 2, 'btn_volume_head'))
         gameScene.volumeSlider[0].visible = false
         gameScene.volumeSlider[1].setScale(2 * VOL_THICKNESS / 800).setInteractive({ draggable: true }).visible = false
@@ -54,7 +54,7 @@ class GameScene extends Phaser.Scene {
         })
 
         // Sound change button
-        this.btnVolume = this.add.sprite(VOL_POS_X, VOL_POS_Y, 'btn_volume').setScale(0.25).setInteractive();
+        this.btnVolume = this.add.sprite(VOL_POS_X, VOL_POS_Y, 'btn_volume').setScale(0.25).setInteractive().setDepth(100);
         this.btnVolume.on('pointerdown', function (pointer) {
             if (gameScene.volumeSlider[0].visible || gameScene.volumeSlider[0].visible) {
                 gameScene.volumeSlider[0].visible = false
@@ -67,7 +67,7 @@ class GameScene extends Phaser.Scene {
         });
 
         // Level change button
-        this.btnLevels = this.add.sprite(90, 90, 'btn_levels').setScale(0.25).setInteractive();
+        this.btnLevels = this.add.sprite(90, 90, 'btn_levels').setScale(0.25).setInteractive().setDepth(100);
         this.btnLevels.on('pointerdown', function (pointer) {
             //var nextLevelIndex = Math.floor(Math.random() * ALL_LEVELS.length) // Test your luck with random level selection
             //gameScene.scene.restart({ levelIndex: nextLevelIndex }) //TODO ACTUAL LEVEL SELECT MENU
