faa1d498ef0bbf7c03f077f4b67580fa533c225a
Henrykvdb <henrykvdb@hotmail.com>
2020-08-11 23:15:39 +0200

    Move sound button to top left

--- ui.js
+++ ui.js
@@ -1,10 +1,3 @@
-// Volume slider settings
-VOL_POS_X = 900
-VOL_POS_Y = 500
-VOL_THICKNESS = 40
-VOL_LENGTH = 200
-VOL_OFFSET = 70
-
 function UI(gameScene) {
     // Next button
     this.btnNext = gameScene.add.sprite(900, 100, 'btn_next').setScale(0.25).setInteractive().setDepth(100);
@@ -22,7 +15,7 @@ function UI(gameScene) {
     // Sound change button
     buttons = []
     for (var i = 0; i < 4; i++) {
-        var button = gameScene.add.sprite(VOL_POS_X, VOL_POS_Y, 'btn_volume_' + i).setScale(0.25).setInteractive().setDepth(100)
+        var button = gameScene.add.sprite(45, 160, 'btn_volume_' + i).setScale(0.14).setInteractive().setDepth(100)
         buttons.push(button)
         button.visible = false
         button.on('pointerdown', function (pointer) {
@@ -37,8 +30,6 @@ function UI(gameScene) {
     // Level change button
     this.btnLevels = gameScene.add.sprite(90, 90, 'btn_levels').setScale(0.25).setInteractive().setDepth(100);
     this.btnLevels.on('pointerdown', function (pointer) {
-        //var nextLevelIndex = Math.floor(Math.random() * ALL_LEVELS.length) // Test your luck with random level selection
-        //gameScene.scene.restart({ levelIndex: nextLevelIndex }) //TODO ACTUAL LEVEL SELECT MENU
         gameScene.scene.pause();
         gameScene.scene.launch('LevelSelectScene');
     });
