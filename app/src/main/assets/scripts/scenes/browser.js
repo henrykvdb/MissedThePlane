48731bfb109ff984cfddd42f025be4cc5d348e44
winand appels <winnie33winnie33@gmail.com>
2020-08-26 12:32:59 +0200

    small browser improvements

--- app/src/main/assets/scripts/scenes/browser.js
+++ app/src/main/assets/scripts/scenes/browser.js
@@ -105,8 +105,8 @@ var createPanel = function (scene) {
     return sizer
 }
 
-var COLOR_LIGHT = "0xD5C175"
-var COLOR_DARK = "0xBE8E17"
+var COLOR_LIGHT = "0xd5c175"
+var COLOR_DARK = "0xbe8f17"
 var createCard = function (scene, levelData) {
     var startButton = scene.add.sprite(0, 0, 'btn_playtest_0').setScale(0.3 * MIN_XY / 600).setInteractive().setDepth(100)
     startButton.on('pointerdown', () => {
@@ -115,23 +115,32 @@ var createCard = function (scene, levelData) {
         scene.scene.start('GameScene', {levelIndex: levelData.id, levelString: levelData.levelString, public: true, levelName: levelData.name})
     })
 
+    var date = new Date(1970, 0, 1); // Epoch
+    date.setSeconds(levelData.submitDate);
+    date = date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate() // eventually add hours?
+
+    var nameSizer = scene.rexUI.add.sizer({orientation: 'y', space: { left: 20, right: 20, top: 20, bottom: 20, item: 10 }});
+    nameSizer.add(scene.add.text(0, 0, levelData.name,{ fill: '#000000', fontSize: 30 * MIN_XY / 600, fontStyle: 'bold' }))
+    .add(scene.add.text(0, 0, "by " + levelData.authorName, { fill: '#000000', fontSize: 20 * MIN_XY / 600}))
+    .add(scene.add.text(0, 0, date, { fill: '#000000', fontSize: 20 * MIN_XY / 600}))
+    .setDepth(100)
+
     return scene.rexUI.add.sizer({
         orientation: 'x',
         space: { left: 20, right: 20, top: 20, bottom: 20, item: 10 }
     })
-    .addBackground(scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(getXY(0.004), COLOR_LIGHT, 1))
-    .add(scene.rexUI.add.roundRectangle(0, 0, getXY(0.2), getXY(0.2), 5, COLOR_LIGHT))
-
-    .add(scene.add.text(0, 0, levelData.name + "\nby " + levelData.authorName,{ fill: '#000000', fontSize: 20 * MIN_XY / 600, fontStyle: 'bold' }))
+    .addBackground(scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, COLOR_LIGHT).setStrokeStyle(getXY(0.01), COLOR_DARK, 1))
+    .add(scene.rexUI.add.roundRectangle(0, 0, getXY(0.2), getXY(0.2), 5, COLOR_DARK))
+    .add(nameSizer)
+    
     .addSpace()
+    .add(scene.add.rectangle(0, 0, getXY(0.008), getXY(0.2), COLOR_DARK))
 
-    .add(scene.add.text(0, 0, levelData.clears + "/" + levelData.plays, { fill: '#000000', fontSize: 20 * MIN_XY / 600, fontStyle: 'bold' }))
-    .add(scene.add.rectangle(0, 0, getXY(0.008), getXY(0.2), COLOR_LIGHT))
+    .add(scene.add.text(0, 0, " " + levelData.clears + "\n---\n " + levelData.plays, { fill: '#000000', fontSize: 20 * MIN_XY / 600, fontStyle: 'bold' }))
+    .add(scene.add.rectangle(0, 0, getXY(0.008), getXY(0.2), COLOR_DARK))
 
     .add(scene.add.text(0, 0, levelData.upvotes + "up\n" + levelData.downvotes + "down",{ fill: '#000000', fontSize: 20 * MIN_XY / 600, fontStyle: 'bold' }))
-    .add(scene.add.rectangle(0, 0, getXY(0.008), getXY(0.2), COLOR_LIGHT))
-
-    .add(scene.add.text(0, 0, levelData.submitDate, { fill: '#000000', fontSize: 20 * MIN_XY / 600, fontStyle: 'bold' }))
+    .add(scene.add.rectangle(0, 0, getXY(0.008), getXY(0.2), COLOR_DARK))
     .addSpace()
 
     .add(startButton)
